fe06a4c788366bae2d3ce0e867ad0a7fe22dd6f1
SQL Junkey <test@test.com>
2023-04-29 21:50:31 +0000

    new

--- Index.hs
+++ Index.hs
@@ -1,3 +1,27 @@
 module Index where
+import Download
 
-compose::[String]->String
+--Open Files
+openFile::String->IO [String]
+openFile a = do 
+            com<-readFile a 
+            let stock =  lines com
+            a<- accumulatePrice stock
+            print (a/ fromIntegral (length stock))
+            return $ lines com
+
+
+--Accumulate Price
+accumulatePrice::[String]->IO Double
+accumulatePrice [] = return 0.0
+accumulatePrice (x:xs) = do 
+                           (a,b,c,d) <- getTicker x
+                           let f = read c :: Double
+                           s <- accumulatePrice xs
+                           return (f + s)
+
+--Presets
+presetIndex::String->IO String
+presetIndex "hammond" = return "IntC Price,Change" 
+
+
