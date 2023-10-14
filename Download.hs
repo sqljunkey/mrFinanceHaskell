bbb82bb27f775680b755cecfc16ca3eaf35fb4a3
SQL Junkey <test@test.com>
2023-10-14 18:08:36 +0000

    changed to TDFA Regex

--- Download.hs
+++ Download.hs
@@ -4,18 +4,28 @@ module Download where
 import Network.Connection (TLSSettings (..))
 import Network.HTTP.Conduit
 import Text.HTML.TagSoup
-import Text.Regex.PCRE
+--import Text.Regex.PCRE
+import Text.Regex.TDFA
 import Text.Read
 import Data.List.Utils
 import Data.List
 import Data.List.Split
 import Data.Char
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
 import qualified Network.HTTP.Types as HTypes
 import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy.Char8 as L8
 
 -- Remove punctuation from text String.
 removePuncNR :: String -> String
-removePuncNR xs = [x | x <- xs, not (x `elem` ",+?!:;nr\\\"\'")]
+removePuncNR xs = [x | x <- xs, not (x `elem` ",+?!:;nr\'\n'")]
+
+removeNewLine :: String -> String
+removeNewLine xs = [x | x <- xs, not (x `elem` "'\n'")]
 
 -- Remove punctuation from text String.
 removePuncc :: String -> String
@@ -26,7 +36,7 @@ removePuncc xs = [x | x <- xs, not (x `elem` ",+?!:;-\\\"\'")]
 --ticker
 mwUrl::String->String->String->String
 mwUrl a "b" "q" = "https://www.marketwatch.com/investing/stock/" ++ a ++ "/financials/balance-sheet/quarter"
-mwUrl a "i" "q" = "https://www.marketwatch.com/investing/stock/" ++ a ++ "/financials//income/quarter"                                 
+mwUrl a "i" "q" = "https://www.marketwatch.com/investing/stock/" ++ a ++ "/financials/income/quarter"                                 
 mwUrl a "c" "q" = "https://www.marketwatch.com/investing/stock/" ++ a ++ "/financials/cash-flow/quarter"
 mwUrl a "b" _ = "https://www.marketwatch.com/investing/stock/" ++ a ++ "/financials/balance-sheet"
 mwUrl a "i" _ = "https://www.marketwatch.com/investing/stock/" ++ a ++ "/financials"                                 
@@ -35,7 +45,7 @@ mwUrl a "c" _ = "https://www.marketwatch.com/investing/stock/" ++ a ++ "/financi
 --cUrl
 
 cUrl::String
-cUrl = "https://coinmarketcap.com/all/views/all/"
+cUrl = "https://coinmarketcap.com/"
 
 
 ---give ticker name and it returns url String
@@ -45,9 +55,14 @@ yfUrl a = "https://finance.yahoo.com/quote/" ++ a
 yfUrlStat::String->String
 yfUrlStat a = "https://finance.yahoo.com/quote/" ++ a ++ "/key-statistics?p="++ a
 
+
+get_tags :: String -> Int -> String
+get_tags html n = take n $ innerText $ dropWhile (~/= "<body>") $ parseTags html
+
+
 --download html 
-downloadHtml :: String->Bool->Int-> IO  String
-downloadHtml a b n = do
+downloadHtml :: String->Int-> IO  String
+downloadHtml a n = do
     request <- parseRequest a 
     let request' = request {requestHeaders = [(HTypes.hUserAgent, C8.pack "HTTP-Conduit")]}
    
@@ -55,16 +70,17 @@ downloadHtml a b n = do
     manager <- newManager settings
     res <- httpLbs request' manager 
     
-    let justText =  (fromFooter  $ parseTags $ show res)
+                       
 
-    --putStrLn $ show $ justText
-    if b == True then
-      return $ show $ justText 
-    else
-      return  $ show $ justText
-     
-        where fromFooter =  innerText  .take n . dropWhile (~/= "<body>")
-     
+    putStrLn $ get_tags (L8.unpack $ responseBody res) n
+
+    return $ get_tags (L8.unpack $ responseBody res) n
+
+
+
+
+
+
 --Stock Data Record
 data StockData = StockData { companyName :: String  
                      , ticker :: String  
@@ -119,20 +135,64 @@ currencyList::String
 currencyList = "(USD|CAD|USX|JPY|HKD|SGD|AUD|SEK|MXN|CNY|TRY)"
 
 
+
+extractNumberFromText :: String -> String
+extractNumberFromText text = go text ""
+  where
+    go [] result = reverse result
+    go (c:cs) result
+        | isDigit c = go cs (c : result)
+        | c == '.' && not (null result) = goDecimal cs (c : result)
+        | otherwise = go cs result
+
+    goDecimal cs result = goDecimal' cs 2 (result)
+    goDecimal' cs 0 result = reverse result
+    goDecimal' [] _ result = reverse result
+    goDecimal' (c:cs) n result
+        | isDigit c = goDecimal' cs (n - 1) (c : result)
+        | otherwise = reverse result
+
+
+
+
+
+
 --do regex on yahoo web string      
 parseHtml::String ->String->Maybe StockData
 parseHtml a tick = 
-    let priceList =  a=~("((\\d+,)*?\\d+.\\d+)((-|\\+)?(\\d+,)?\\d+.\\d+)\\s+\\(((-|\\+)?\\d+.\\d+)%\\)")::[[String]]
-        companyName = a=~("((\\w|\\s|\\.|,|\\^|\\&|')+)\\("++ processTicker tick ++"\\)")::[[String]]
-        dividend=  a=~"Dividend & Yield\\d+.\\d+\\s+\\((\\d+.\\d+%)\\)"::[[String]]
-        marketCap =  a=~"Market Cap(\\d+\\.\\d+(B|T|M))"::[[String]]
-        afterHours =   a=~"\\(((-|\\+)\\d+.\\d+)%\\)After hours"::[[String]]
-        peRatio = a=~"PE\\s+Ratio\\s+\\(TTM\\)(\\d+.\\d+)"::[[String]]
-        weeksChange = a=~"52\\s+Week\\s+Range((\\d+,)*?\\d+.\\d+\\s+-\\s+(\\d+,)*?\\d+.\\d+)"::[[String]]
-        beta        = a=~"Beta\\s+\\(5Y Monthly\\)\\s+(\\d+\\.\\d+)52"::[[String]]
-        volume      = a=~"Volume((\\d+,)+\\d+)"::[[String]]
-        avgVolume   = a=~"Avg. Volume((\\d+,)+\\d+)"::[[String]]
-        priceSale   = a=~"Price\\/Sales\\s+\\(ttm\\)(\\d+\\.\\d\\d)"::[[String]]
+    let priceList =  a=~("(([0-9]+,)?[0-9]+.[0-9]+)((-|\\+)?([0-9]+,)?[0-9]+.[0-9]+)"
+                        ++space
+                        ++"\\(((-|\\+)?[0-9]+.[0-9]+)%\\)")::[[String]]
+        companyName = a=~("(([A-Z]|[a-z]|\\.|,|\\^|\\&|'|"
+                        ++space
+                        ++")+)\\("++ processTicker tick ++"\\)")::[[String]]
+        dividend=  a=~ ("Dividend & Yield[0-9]+\\.[0-9]+"
+                      ++space
+                      ++"\\(([0-9]+\\.[0-9]+\\%)\\)")::[[String]]
+        marketCap =  a=~"Market Cap([0-9]+\\.[0-9]+(B|T|M))"::[[String]]
+        afterHours =   a=~"\\(((-|\\+)?[0-9]+.[0-9]+)%\\)After hours"::[[String]]
+        peRatio = a=~("PE"
+                    ++space
+                    ++"Ratio"
+                    ++space
+                    ++"\\(TTM\\)([0-9]+.[0-9]+)")::[[String]]
+        weeksChange = a=~("52"
+                    ++space
+                    ++"Week"
+                    ++space
+                    ++"Range(([0-9]+,)?[0-9]+.[0-9]+"
+                    ++space
+                    ++"-"
+                    ++space
+                    ++"([0-9]+,)?[0-9]+.[0-9]+)")::[[String]]
+        beta        = a=~("Beta"
+                    ++space
+                    ++"\\(5Y Monthly\\)([0-9]+\\.[0-9]+)")::[[String]]
+        volume      = a=~"Volume(([0-9]+,)+[0-9]+)"::[[String]]
+        avgVolume   = a=~"Avg. Volume(([0-9]+,)+[0-9]+)"::[[String]]
+        priceSale   = a=~("Price\\/Sales"
+                        ++space
+                        ++"\\(ttm\\)([0-9]+.[0-9][0-9])")::[[String]]
        
 
     in  if priceList== [] || companyName == [] then
@@ -141,27 +201,24 @@ parseHtml a tick =
         Just(StockData{ companyName = ( listToString companyName  1) 
                        ,ticker = tick
                        ,price = (listToString priceList  1)
-                       ,percentageChange = (listToString priceList 6)
-                       ,dividend= (listToString dividend  1) 
+                       ,percentageChange = (listToString priceList 7)
+                       ,dividend= (listToString dividend  2) 
                        ,marketCap = (listToString marketCap  1)
                        ,afterHours =(listToString afterHours 1)
-                       ,peRatio =(listToString peRatio 1) 
-                       ,weeksChange = (listToString weeksChange 1)
-                       ,beta =(listToString beta 1)
+                       ,peRatio =(listToString peRatio 3) 
+                       ,weeksChange = (listToString weeksChange 3)
+                       ,beta =(listToString beta 2)
                        ,volume =(listToString volume 1)
                        ,avgVolume=(listToString avgVolume 1)
-                       ,priceSale=(listToString priceSale 1)
+                       ,priceSale=( listToString priceSale 2)
                        
                         })
 
 
-    --get ticker with simple information
+--get ticker with simple information
 getTicker :: String -> IO StockData
 getTicker tick  = do
-  str' <- downloadHtml  (yfUrl tick) True 1500
-  
-
-
+  str' <- downloadHtml  (yfUrl tick) 6000
   case parseHtml str' tick of
     Nothing -> pure StockData{companyName ="" 
                        ,ticker = tick
@@ -182,8 +239,8 @@ getTicker tick  = do
     --get ticker with extended information 
 getTickerStat :: String -> IO StockData
 getTickerStat tick  = do
-  str' <- downloadHtml  (yfUrl tick) True 1500
-  str1' <- downloadHtml  (yfUrlStat tick) True 1500
+  str' <- downloadHtml  (yfUrl tick) 6000
+  str1' <- downloadHtml  (yfUrlStat tick) 6000
 
 
   case parseHtml (str'++str1') tick of
@@ -212,7 +269,7 @@ formatSpace = foldr go ""
 --get type
 getType::String->IO String 
 getType tick = do 
-              str1 <- downloadHtml ("https://www.marketwatch.com/investing/stock/"++tick) False 10000
+              str1 <- downloadHtml ("https://www.marketwatch.com/investing/stock/"++tick) 30000
               --putStrLn  $ show  str1
 
               let final = str1 =~("(([A-Z]|[a-z]|\\.|\\d|\\s|,|-|\\&|\\')+"
@@ -232,72 +289,75 @@ getTypeF  (x:xs) =  lts x 1 ""
 
 --(get Rev, Cash,Debt,Asset )
 getSheets::String->String->String-> IO [String]
-getSheets c tick quarter= do
-       str1 <- downloadHtml  (mwUrl tick "b"  quarter) False 10000
-       str2 <- downloadHtml  (mwUrl tick "c"  quarter) False 10000
-       str3 <- downloadHtml  (mwUrl tick "i"  quarter) False 10000
+getSheets nick tick quarter= do
+       str1 <- downloadHtml  (mwUrl tick "b"  quarter) 5000000
+       str2 <- downloadHtml  (mwUrl tick "c"  quarter) 5000000
+       str3 <- downloadHtml  (mwUrl tick "i"  quarter) 5000000
         
-       let test = parseSheetHtml $ removePuncNR ( str1 ++ str2 ++ str3)
+       let html = parseSheetHtml $formatSpace $ removeNewLine $  str1++ str2 ++ str3
       
-       putStrLn  $ show $ removePuncNR str3
-       return $ replaceStuffs c $ flattenSheet test
+       putStrLn $ show html
+
+       --putStrLn  $ show $ removePuncNR str3
+       return $ formatSheets nick $ firstElement html
+
+space::String
+space ="(\\t|\\n|\\r|\\v|\\f| )+"
+
+numbers::String
+numbers = "("++space++"\\(?([0-9]+|[0-9]+.[0-9]+)+(B|M|K|T)\\)?)+"
+
+percentage::String
+percentage = "("++space++"-?([0-9]+|[0-9]+.[0-9]+)?(%|-)?)+"
 
 
-columnParse::String
-columnParse = "\\s+(\\s+\\(?((\\d+.\\d+)|(\\d)|(\\d+))(T|B|M|K)\\)?)+"
-
---Parse balance sheet data
 parseSheetHtml::String->[[String]]
 parseSheetHtml a = 
-                let totalAsset =a =~("(Total\\s+Assets)" ++ columnParse) ::[[String]]
-                    totalLiabilities = a =~ ("(Total\\s+Liabilities)"++columnParse) ::[[String]] 
-                    freeCashflow = a =~ ("(Fee\\s+Cash\\s+Flow)"++columnParse) ::[[String]] 
-                    opCashflow  = a =~ ("(Net\\s+Opeatig\\s+Cash\\s+Flow)"++columnParse) ::[[String]] 
-                    revenue  = a =~ ("(Reveue)"++columnParse) ::[[String]] 
-                    netIncome  = a =~ ("(Net\\s+Icome)"++columnParse) ::[[String]] 
-                    interstIncome = a =~ ("(Iteest\\s+Icome)"++columnParse) ::[[String]] 
-                    cogs = a =~ ("(Goss\\s+Icome\\s+Gowth)"++"\\s+(\\s+\\(?-?((\\d+.\\d+)|(\\d)|(\\d+)|-)(T|B|M|K|%)?\\)?)+") ::[[String]] 
+                let totalAsset =a =~("Total"++space++"Assets"++numbers) :: [[String]]
+                    totalLiabilities = a =~ ("Total"++space++"Liabilities"++numbers) ::[[String]] 
+                    freeCashflow = a =~ ("Free"++space++"Cash"++space++"Flow"++numbers) ::[[String]] 
+                    opCashflow  = a =~ ("Net"++space++"Operating"++space++"Cash"++space++"Flow"++numbers) ::[[String]] 
+                    revenue  = a =~ ("Revenue"++numbers) ::[[String]] 
+                    netIncome  = a =~ ("(Net"++space++"Income)"++numbers) ::[[String]] 
+                    interstIncome = a =~ ("Interest"++space++"Income"++numbers) ::[[String]] 
+                    cogs = a =~ ("Gross"++space++"Income"++space++"Growth"++percentage) ::[[String]] 
                in 
                   revenue
-                ++[if length (interstIncome) > 0 && length (revenue) == 0 then   interstIncome !! 0 else [] ]
-                ++ [if length (netIncome) > 0 then   netIncome !! 0 else []]
-                ++ cogs
-                ++ opCashflow 
-                ++ freeCashflow 
-                ++totalAsset 
-                ++ totalLiabilities 
+                 ++[if length (interstIncome) > 0 && length (revenue) == 0 then   interstIncome !! 0 else [] ]
+                 ++ [if length (netIncome) > 0 then   netIncome !! 0 else []]
+                 ++ cogs
+                 ++opCashflow
+                 ++freeCashflow 
+                 ++totalAsset
+                 ++ totalLiabilities
                 
---Flatten List
-flattenSheet::[[String]]->[String]
-flattenSheet [] = []
-flattenSheet (x:xs) =    [if length x>0 then formatSpace $ x !! 0 else []] ++  flattenSheet xs
 
---Replace Items In GetSheet                                   
-replaceStuffs::String->[String]->[String]
-replaceStuffs c [] = []
-replaceStuffs c (x:xs)| "Iteest Icome" `isInfixOf` x = [replace "Iteest Icome" ("PRIVMSG:"++c++" :Interest Income") x] ++ replaceStuffs c xs 
-replaceStuffs c (x:xs)| "Reveue" `isInfixOf` x = [replace "Reveue" ("PRIVMSG:"++c++" :Revenue") x] ++ replaceStuffs c xs   
-replaceStuffs c (x:xs)| "Net Opeatig Cash Flow" `isInfixOf` x = [replace "Net Opeatig Cash Flow" ("PRIVMSG:"++c++" :Net Operatig Cash Flow") x] ++ replaceStuffs c xs          
-replaceStuffs c (x:xs)| "Fee Cash Flow" `isInfixOf` x = [replace "Fee Cash Flow" ("PRIVMSG:"++c++" :Free Cash Flow") x] ++ replaceStuffs c xs
-replaceStuffs c (x:xs)| "Net Icome" `isInfixOf` x = [replace "Net Icome" ("PRIVMSG:"++c++" :Net Income") x] ++ replaceStuffs c xs
-replaceStuffs c (x:xs)| "Goss Icome Gowth" `isInfixOf`x = [replace "Goss Icome Gowth" ("PRIVMSG:"++c++" :Gross Income Growth") x] ++ replaceStuffs c xs
-replaceStuffs c (x:xs) = [("PRIVMSG:"++c++" :"++x)] ++ replaceStuffs c xs
+firstElement::[[String]]->[String]
+firstElement [] = []
+firstElement (x:xs) =    [if length x>0 then formatSpace $ x !! 0 else []] ++  firstElement xs
+
+                                  
+formatSheets :: String -> [String] -> [String]
+formatSheets nick [] = []
+formatSheets nick (x:xs)
+    | null x = formatSheets nick xs
+    | otherwise = ("PRIVMSG:" ++ nick ++ " :" ++ x) : formatSheets nick xs
 
 --Parse Crypto
 parseCrypto::String->String->Maybe StockData 
-parseCrypto a s = let b =s =~((map toUpper a) 
-                       ++"(\\w+(\\s+\\w+)?)"
+parseCrypto a s = let b =s =~("(([A-Z]+)?([a-z]+)?)+"
                        ++(map toUpper a)
-                       ++"\\$.*?\\$.*?(\\$(\\d+\\,)?\\d+\\.\\d(\\.+\\d)?\\d).*?"
-                       ++(map toUpper a)
-                       ++".*?(-?\\d+\\.\\d+)\\%(-?\\d+\\.\\d+)\\%(-?\\d+\\.\\d+)\\%")
-                       in if b == [] then
+                       ++"(\\$(([0-9]+),)?[0-9]+.[0-9][0-9])"
+                       ++"(-?[0-9]+.[0-9][0-9])\\%"
+                       ++"(-?[0-9]+.[0-9][0-9])\\%")
+                      
+                       in if b==[] then
                         Nothing
                         else
                           Just (StockData{companyName=listToString b  1 
                                           ,ticker =a 
-                                          ,price = listToString b  3 
-                                          ,percentageChange= if ("-" `isPrefixOf`  (listToString b  7)) then (listToString b  7) else "+"++(listToString b  7)
+                                          ,price = listToString b  4
+                                          ,percentageChange= listToString b 8
                                           ,dividend=listToString b  2 
                                           ,marketCap=listToString b  2 
                                           ,peRatio=listToString b  2 
@@ -311,7 +371,7 @@ parseCrypto a s = let b =s =~((map toUpper a)
 
 getCrypto::String->IO StockData
 getCrypto tick  = do
-  str' <- downloadHtml  cUrl False 4000
+  str' <- downloadHtml  cUrl 5000
   case parseCrypto tick (str')of
      Nothing -> pure StockData{companyName ="" 
                        ,ticker = tick
