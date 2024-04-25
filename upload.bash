879b3d84b3ac41b79cc084f388cb8b631470551d
sqljunkey <ddjames091984@gmail.com>
2024-04-24 19:43:15 -0500

    jokes

--- upload.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pscp.exe -pw BadAss C:/Users/Adian/haskell/* mrfinance@launsdorf.com:/home/mrfinance/testcode/
--- upload.bat
+++
@@ -1,1 +0,0 @@
-pscp -pw BadAss c:/Users/Adian/haskell/* mrfinance@launsdorf.com:/home/mrfinance/testcode/ 
